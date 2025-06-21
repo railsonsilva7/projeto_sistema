a3cedada13ab94fe8e7c1792b16fd2c1554bc198
Railson de Oliveira Silva <railson.de.oliveira.silva.ros@gmail.com>
2025-06-21 00:32:52 -0300

    Primeiro componente

---
+++ meu-projeto-react/src/MeuComponente.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function MeuComponente() {
+    return (
+        <div id='meu-componente'>
+            <h2>Meu Componente</h2>
+            <p>Criado manualmente com react</p>
+        </div>
+    );
+}
+
+export default MeuComponente;
